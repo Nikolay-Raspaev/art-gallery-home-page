8946c896f79cdcaf89af6d6e3dc7a1eb5601dac6
Николай <raspaev.niko@mail.ru>
2023-08-11 00:02:21 +0400

    fix

--- src/store/Api/paintingApi.ts
+++ src/store/Api/paintingApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { IPainting } from '../../types/interfaces';
 import { getPageCount } from '../../utils/getPageCount';
-import { removeEmptyKeys } from '../reducers/removeEmptyKeys';
+import { removeEmptyKeys } from '../../utils/removeEmptyKeys';
 import { IIncomingParamsForPaintings } from '../../types/types';
 
 export const paintingApi = createApi({
