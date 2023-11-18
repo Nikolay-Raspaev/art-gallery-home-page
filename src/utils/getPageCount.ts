d0c2b83a6f1111ea68a443317b1e5fbd2029131b
Николай <raspaev.niko@mail.ru>
2023-11-18 18:30:34 +0400

    Updates

--- src/utils/getPageCount.ts
+++
@@ -1,3 +0,0 @@
-import { LIMIT } from '../constants';
-
-export const getPageCount = (totalCount: number): number => Math.ceil(totalCount / LIMIT);
--- src/utils/removeEmptyKeys.ts
+++
@@ -1,3 +0,0 @@
-export const removeEmptyKeys = (obj: object) => {
-    return Object.fromEntries(Object.entries(obj).filter(([_, value]) => value));
-};
